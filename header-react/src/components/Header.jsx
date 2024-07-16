97591ad4d19d5fecc00bedefc7308526b8b932b5
GuilhermeRCabreira <guilhermercabreira@gmail.com>
2024-07-15 21:53:48 -0300

    Header pronto

---
+++ header-react/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css";
+function Header() {
+  return (
+    <div>
+      <nav>
+        <strong>Primeiro Componente</strong>
+        <div>
+          <a href="">Pagina Inicial</a>
+          <a href="">Sobre</a>
+          <a href="">Contato</a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
